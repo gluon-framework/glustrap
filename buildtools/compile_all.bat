d8802684a5e22553f5977670bee9d0b09dd0bb61
CanadaHonk <oj@oojmed.com>
2023-04-03 15:18:00 +0100

    linux arm64 support (wip)

--- buildtools/compile_all.bat
+++ buildtools/compile_all.bat
@@ -1,10 +1,13 @@
+rmdir /s /Q bin
 mkdir bin
+
 nim -d:release --gc:none --opt:size --app:gui -o:bin\win_x64.exe c main.nim
 call nimxc -d:release --gc:none --opt:size --app:gui -o:bin\win_x86.exe c main.nim -t windows-i386
 call nimxc -d:release --gc:none --opt:size --app:gui -o:bin\win_arm64.exe c main.nim -t windows-arm64
 
 call nimxc -d:release --gc:none --opt:size --app:gui -o:bin\linux_x64 c main.nim -t linux-amd64
 call nimxc -d:release --gc:none --opt:size --app:gui -o:bin\linux_x86 c main.nim -t linux-i386
+call nimxc -d:release --gc:none --opt:size --app:gui -o:bin\linux_arm64 c main.nim -t linux-arm64
 
 call nimxc -d:release --gc:none --opt:size --app:gui -o:bin\mac_x64 c main.nim -t macosx-amd64
 call nimxc -d:release --gc:none --opt:size --app:gui -o:bin\mac_arm64 c main.nim -t macosx-arm64
--- buildtools/compile_linux.sh
+++ buildtools/compile_linux.sh
@@ -8,5 +8,8 @@ nim -d:release --gc:none --opt:size --app:gui -o:bin/linux_x64 c main.nim
 # linux x86 (cross)
 nimxc -d:release --gc:none --opt:size --app:gui -o:bin/linux_x86 c main.nim -t linux-i386
 
+# linux arm64 (cross)
+nimxc -d:release --gc:none --opt:size --app:gui -o:bin/linux_arm64 c main.nim -t linux-arm64
+
 # strip all bins
 strip -s bin/*
